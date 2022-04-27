da3e87040ed2cd466fa933ef9a64eabc793008c3
foforane <thakgalang.cyber@gmail.com>
2022-04-27 06:57:30 +0200

    Practice times

--- index.php
+++ index.php
@@ -7,12 +7,17 @@
     <title>SMU</title>
     <!-- CSS only -->
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
 </head>
+
 <body>
 <img src="logo.png" height="100" alt="Sample photo">
-    <h1>SMU Lichess Management System</h1>
+    <h1>SMU Lichess Rank</h1>
     <p>This list is sorted from best going down, The best player is selected using the Blitz rating, as many serious games are played in blitz!</p>
-  
+    <h2>Official Practice times</h2>
+    <h3>Venue : Sports Comm</h3>
+    <p>Thursday 16h30 - 18h00</p>
+    <p>Friday 16h30 - 18h00</p>
     <div class="table-responsive">
  <table class="table table-hover filterTable">
   <thead>
@@ -24,6 +29,7 @@
       <th scope="col">Blitz</th>
       <th scope="col">Bullet</th>
       <th scope="col">Rapid</th>
+      <th scope ="col">Section</th>
       <th scope="col">Phone Number</th>
       <th scope="col">Username</th>
 
@@ -37,6 +43,7 @@ $sql = "SELECT * FROM details";
 $result = $conn->query($sql);
 $users = [];
 $bestplayerinfo = "";
+$femaleInfo = "";
 if($result->num_rows>0){
     while($row = $result->fetch_assoc()) {
     array_push($users,$row);
@@ -47,6 +54,8 @@ $conn->close();
 $risk = [];
 $games = 0;
 $bestBlitz = 0;
+$bestFemale = 0;
+$bestuserName ="";
 $ch = curl_init();
 foreach($users as $user){
     
@@ -67,10 +76,22 @@ if($Blitz <= 1400){
     $riskplayer = $user['surname']." ".$user['name']." with a blitz rating of ".$Blitz;
     array_push($risk,$riskplayer);
 }
+if($user['section'] == "Female"){
+  if($bestFemale < $Blitz){
+  $bestFemale = $Blitz;
+  $games = $res->perfs->blitz->games;
+
+  $femaleInfo = $user['surname']." ".$user['name']." with a blitz rating of ".$Blitz." and ".$games." blitz  games played."
+  ?>
+  
+ <?php }
+}
 if($bestBlitz < $Blitz){
 $bestBlitz = $Blitz;
 $games = $res->perfs->blitz->games;
-$bestplayerinfo = $user['surname']." ".$user['name']." with a blitz rating of ".$Blitz." with ".$games." blitz games played.";
+$bestuserName =$user['username'];
+$bestplayerinfo = $user['surname']." ".$user['name']." with a blitz rating of ".$Blitz." and ".$games." blitz games played. 
+if you want to Dethrone ".$user['name']." you can request a ";
 }
 ?>
 
@@ -84,7 +105,8 @@ $bestplayerinfo = $user['surname']." ".$user['name']." with a blitz rating of ".
 <td><?php echo $Blitz; ?></td>
 <td><?php echo $bullet; ?></td>
 <td><?php echo $rapid; ?></td>
-<td><?php echo $user['number']; ?></td>
+<td><?php echo $user['section']?></td>
+<td><?php echo "0". $user['number']; ?></td>
 <td><?php echo $user['username']; ?></td>
 </tr>
   
@@ -128,17 +150,22 @@ curl_close($ch);
 </script>
 
 <h2>Best:</h2>
+ <p><?php echo $bestplayerinfo;?> 
+ <a href = "https://lichess.org/@/<?php echo $bestuserName;?>" class = "link-primary ">Challange</a>
+</p>
 
-<?php
-echo "<p>".$bestplayerinfo."</p>";
+  
+<h2>The best female player is:</h2>
+<?php echo $femaleInfo."<br>";
 if(!empty($risk)){
-    echo "<h2>Players who are in the critical area (blitz <= 1400):</h2>";
+  echo "<h2>Players who are in the critical area (blitz <= 1400):</h2>";
 
 foreach($risk as $r){
-    echo "<p>".$r."</p>";
+  echo "<p>".$r."</p><br>";
 }
 }
 ?>
 <a href = "https://lichess.org/team/team-smu" class = "btn btn-primary">Join SMU Team on Lichess</a>
+<p>Disclaimer: This list will not be used in any selections for over the board activities. It remains strictly Lichess.</p>
 </body>
 </html>
